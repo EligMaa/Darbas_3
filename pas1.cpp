3b71fadea10456be3ebf68d4359f74694b8fe252
EligMaa <151032480+EligMaa@users.noreply.github.com>
2024-02-12 18:58:07 +0200

    Add files via upload

--- pas1.cpp
+++ pas1.cpp
@@ -3,6 +3,8 @@
 #include <iomanip>
 #include <math.h>
 #include <string>
+#include <algorithm>
+
 
 using namespace std;
 
@@ -12,11 +14,18 @@ struct studentas
     string Pavarde;
     int egz_rez;
     int pazKiekis;
-    int* tarpiniai;                         ///rodykle i masyvo adresa
+    int* tarpiniai;                         ///rodykle i masyvo adresa sukis saugos namu darbu rezultatus
     double galutinis;
+    double mediana;
+
 };
 
-void spausdinimas();
+void spausdinimas(int &m, studentas var[]);
+double galutinis(studentas var[], int &k,int &pasirinkimas);
+double mediana(studentas var[],int &k);
+double vidurkis(studentas var[],int &k);
+
+
 
 
 int main() {
@@ -26,7 +35,7 @@ int m;
 
 cout<<"Iveskite kiek yra studentu: ";
 cin >> m;
-studentas* var = new studentas [m];
+studentas* var = new studentas [m];                                /// rodyklė rodanti i dinamineje atmintyje sukurta strukturu masyva
 
 
 for (int i =0; i<m;i++){
@@ -41,29 +50,25 @@ cin>>var[i].Pavarde;
 cout<<"Iveskite kiek namu darbu uzduociu atliko studentas: ";
 cin>>n;
 
+var[i].pazKiekis=n;
 var[i].tarpiniai = new int [n];
 
 cout<<" Iveskite studento namu darbu rezultatus: "<<endl;
 for(int j=0; j<n;j++){
-
 cin>>var[i].tarpiniai[j];
-
+sum+=var[i].tarpiniai[j];
 }
 
-cout<<" Iveskite "<<i+1<<" vartotojo ezamino rezultata:";
+cout<<" Iveskite "<<i+1<<" studento ezamino rezultata:";
 cin>>var[i].egz_rez;
 
-var[i].galutinis=(sum/n)*0.4+0.6+var[i].egz_rez;
+
+// var[i].galutinis=(sum/n)*0.4+0.6*var[i].egz_rez;
 cout<<endl;
 
 }
 
-cout<<"Pavarde\t\t Vardas\t\t Galutinis (Vid.)"<<endl;
-cout<<"-------------------------------------------------------"<<endl;
-for(int i=1;i<=m; i++){
-
-   // cout<<
-}
+spausdinimas(m, var);
 
 delete[] var;
 delete[] var->tarpiniai;
@@ -72,9 +77,60 @@ delete[] var->tarpiniai;
 }
 
 
-void spausdinimas(){
+void spausdinimas(int &m,studentas var[]){
+
+    int pasirinkimas; 
+    cout<<"Jei norite spausdinti studentu galutini vidurki iveskite: 0\nJei norite spausdinti studentu galutine mediana iveskite: 1"<<endl;
+    cin>>pasirinkimas;
+
+    cout<< "Pavarde"<<setw(10)<<" Vardas"<<setw(10)<<"Galutinis (Vid.)"<<endl;
+    cout<<"-------------------------------------------------------"<<endl;
 
     
+
+    for(int i=0;i<m; i++){
+
+       cout<<var[i].Vardas;
+       cout<<setw(10)<<var[i].Pavarde;
+       cout<<setw(20)<< setprecision(3)<<galutinis(var,i,pasirinkimas)<<endl;
+    }
+
+ 
+
+    //    cout<<mediana(var,i);
+
+}
+
+double galutinis(studentas var[], int &k,int &pasirinkimas){
+
+    double gal;
+    if(pasirinkimas==0){
+     gal=vidurkis(var, k)*0.4+0.6*var[k].egz_rez;
+    }
+
+    else{
+        gal=(mediana(var, k))*0.4+0.6*var[k].egz_rez;
+    }
+
+    return gal;
+}
+
+
+double mediana(studentas var[],int &k){
+
+    int sk=var[k].pazKiekis;
+    
+}
+
+double vidurkis(studentas var[],int &k){
+    double sum=0.0;
+
+    for(int j=0; j<var[k].pazKiekis;j++){
+    sum+=var[k].tarpiniai[j];
+    }
+    double vid = sum/var[k].pazKiekis;
+
+    return vid;
 }
 
 
