efb0fa6a28b60101fcfbd4c44ea80fe615a4a588
EligMaa <151032480+EligMaa@users.noreply.github.com>
2024-02-12 17:13:46 +0200

    Add files via upload

--- pas1.cpp
+++ pas1.cpp
@@ -2,46 +2,81 @@
 #include <fstream>
 #include <iomanip>
 #include <math.h>
+#include <string>
+
 using namespace std;
 
-const int n = 3;                                                            ///kiek namu uzduociu kiekis
-const int m = 2;                                                            ///studentu skaicius
-
-struct vartotojas
+struct studentas
 {
     string Vardas;
     string Pavarde;
     int egz_rez;
-    int tarpiniai[n];
+    int pazKiekis;
+    int* tarpiniai;                         ///rodykle i masyvo adresa
+    double galutinis;
 };
+
+void spausdinimas();
 
 
 int main() {
 
-for (int i =1; i<=m;i++){
+int n;
+int m;
 
-vartotojas var_i;
+cout<<"Iveskite kiek yra studentu: ";
+cin >> m;
+studentas* var = new studentas [m];
+
+
+for (int i =0; i<m;i++){
+
 double sum=0.0;
 
-cout<<" Iveskite "<<i<<" vartotojo varda:";
-cin>>var_i.Vardas;
-cout<<" Iveskite "<<i<<" vartotojo pavarde:";
-cin>>var_i.Pavarde;
-cout<<" Iveskite vartotojo namu darbu rezultatus: "<<endl;
+cout<<" Iveskite "<<i+1<<" studento varda:";
+cin>>var[i].Vardas;
+cout<<" Iveskite "<<i+1<<" studento pavarde:";
+cin>>var[i].Pavarde;
+
+cout<<"Iveskite kiek namu darbu uzduociu atliko studentas: ";
+cin>>n;
+
+var[i].tarpiniai = new int [n];
+
+cout<<" Iveskite studento namu darbu rezultatus: "<<endl;
 for(int j=0; j<n;j++){
 
-cin>>var_i.tarpiniai[j];
-sum=+var_i.tarpiniai[j];
+cin>>var[i].tarpiniai[j];
 
 }
-cout<<" Iveskite "<<i<<"  vartotojo ezamino rezultata:";
-cin>>var_i.egz_rez;
 
-cout<<" Galutinis balas: "<<(sum/n)*0.4+0.6+var_i.egz_rez<<endl;
+cout<<" Iveskite "<<i+1<<" vartotojo ezamino rezultata:";
+cin>>var[i].egz_rez;
+
+var[i].galutinis=(sum/n)*0.4+0.6+var[i].egz_rez;
 cout<<endl;
 
 }
 
+cout<<"Pavarde\t\t Vardas\t\t Galutinis (Vid.)"<<endl;
+cout<<"-------------------------------------------------------"<<endl;
+for(int i=1;i<=m; i++){
+
+   // cout<<
+}
+
+delete[] var;
+delete[] var->tarpiniai;
 
 
 }
+
+
+void spausdinimas(){
+
+    
+}
+
+
+
+
