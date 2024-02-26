6bd496a0b066a3ec7a24a8685a8ab0ca590c9f30
Rugile Ma <151032480+EligMaa@users.noreply.github.com>
2024-02-27 00:19:09 +0200

    Add files via upload

---
+++ manoBiblioteka.h
@@ -0,0 +1,37 @@
+#ifndef MANOBIBLIOTEKA_H
+#define MANOBIBLIOTEKA_H
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <ctime>
+#include <sstream>
+#include <chrono>
+#include <stdexcept>
+#include <limits>
+
+
+using std::cout;
+using std::cin;
+using std::setw;
+using std::string;
+using std::left;
+using std::endl;
+using std::vector;
+using std::invalid_argument;
+using std::cerr;
+using std::numeric_limits;
+using std::streamsize;
+using std::ofstream;
+using std::fixed;
+using std::setprecision;
+using std::ifstream;
+using std::istringstream;
+
+
+#endif
