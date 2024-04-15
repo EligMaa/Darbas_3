9bdca24e86fc079156542c04c59abad62f4e162a
Rugile Ma <151032480+EligMaa@users.noreply.github.com>
2024-04-15 18:41:46 +0300

    Add files via upload

--- funkcijos1_5.h
+++ funkcijos1_5.h
@@ -3,11 +3,43 @@
 
 #include "manoBiblioteka.h"
 
-class studentas {
-    private:
+class zmogus {
+    protected:
     
         string Vardas;
         string Pavarde;
+    public:
+        zmogus () : Vardas ( ""), Pavarde ( ""){}
+
+        zmogus( string Var, string Pav ) {
+            Vardas = Var;
+            Pavarde = Pav;
+        }
+
+        /// destruktorius
+        ~zmogus ( ){Vardas.clear(),Pavarde.clear();}
+
+        string set_Vardas (string var){
+            Vardas = var;
+        }
+        string get_Vardas ()const{
+            return Vardas;
+        }
+
+        string set_Pavarde (string pav){
+            Pavarde = pav;
+        }
+        string get_Pavarde ()const{
+            return Pavarde;
+        }
+
+
+
+};
+
+class studentas : public zmogus {
+    private:
+    
         int egz_rez;
         int pazKiekis;
         vector<int> tarpiniai;
@@ -17,25 +49,22 @@ class studentas {
         double gal_med;
 
     public:
-        studentas () : Vardas ( ""), Pavarde ( ""), tarpiniai (), pazKiekis (0), egz_rez (0){}
+        studentas () : tarpiniai (), pazKiekis (0), egz_rez (0){}
 
-        studentas( string Var, string Pav, vector<int> tarp,double egz ) {
-            Vardas = Var;
-            Pavarde = Pav;
+        studentas(string Var, string Pav, vector<int> tarp,double egz ) : zmogus(Var, Pav) {
+            
             tarpiniai = tarp;
             pazKiekis = tarp.size();
             egz_rez = egz;
             pazKiekis = tarpiniai.size();
 
         }
-
+         
         /// destruktorius
-        ~studentas ( ){tarpiniai.clear(), Vardas.clear(),Pavarde.clear();}
+        ~studentas ( ){tarpiniai.clear();}
 
         /// kopijavimo konstruktorius
-        studentas(const studentas& other) {
-            Vardas = other.Vardas;
-            Pavarde = other.Pavarde;
+        studentas(const studentas& other) : zmogus(other.get_Vardas(), other.get_Pavarde()) {
             egz_rez = other.egz_rez;
             tarpiniai = other.tarpiniai;
             vidurkis = other.vidurkis;
@@ -44,11 +73,12 @@ class studentas {
             gal_med = other.gal_med;
         }
 
+
         /// kopijavimo assignment operatorius
         studentas& operator=(const studentas& other) {
             if (this != &other) {
-                Vardas = other.Vardas;
-                Pavarde = other.Pavarde;
+                zmogus :: set_Vardas (other.get_Vardas());
+                zmogus :: set_Pavarde (other.get_Pavarde());
                 egz_rez = other.egz_rez;
                 tarpiniai = other.tarpiniai;
                 vidurkis = other.vidurkis;
@@ -61,8 +91,8 @@ class studentas {
 
         /// perkelimo konstruktorius
         studentas(studentas&& other) noexcept {
-            Vardas = move(other.Vardas);
-            Pavarde = move(other.Pavarde);
+            zmogus :: set_Vardas (move(other.Vardas));
+            zmogus :: set_Pavarde (move(other.Pavarde));
             egz_rez = other.egz_rez;
             tarpiniai = move(other.tarpiniai);
             vidurkis = other.vidurkis;
@@ -74,8 +104,8 @@ class studentas {
         /// perkelimo assignment operatorius
         studentas& operator=(studentas&& other) noexcept {
             if (this != &other) {
-                Vardas = move(other.Vardas);
-                Pavarde = move(other.Pavarde);
+                zmogus::Vardas = std::move(other.Vardas);
+                zmogus::Pavarde = std::move(other.Pavarde);
                 egz_rez = other.egz_rez;
                 tarpiniai = move(other.tarpiniai);
                 vidurkis = other.vidurkis;
@@ -87,27 +117,32 @@ class studentas {
         }
 
         friend ostream& operator<<(ostream& out, const studentas &stud) {                ///isvesties operatorius
-        out << left << setw(15) << stud.Vardas << setw(15) << stud.Pavarde;
+        out << left << setw(15) << stud.get_Vardas() << setw(15) << stud.get_Pavarde();
         out << setw(15) << fixed << setprecision(2) << stud.get_gal_v_m(0);
         out << setw(20) << setprecision(2) << stud.get_gal_v_m(1) << "\n";
         return out;
         }
 
         friend ostream& su_mediana(ostream& out, const studentas& stud) {
-            out << left << setw(15) << stud.Vardas << setw(15) << stud.Pavarde;
+            out << left << setw(15) << stud.get_Vardas() << setw(15) << stud.get_Pavarde();
             out << setw(20) << setprecision(2) << stud.gal_med << "\n";
             return out;
         }
 
         friend ostream& su_vidurkiu(ostream& out, const studentas& stud) {
-            out << left << setw(15) << stud.Vardas << setw(15) << stud.Pavarde;
+            out << left << setw(15) << stud.get_Vardas() << setw(15) << stud.get_Pavarde();
             out << setw(20) << setprecision(2) << stud.gal_vid<< "\n";
             return out;
         }
 
 
         friend istream& operator>>(istream& in, studentas &stud) {                      /// ivesties operatorius
-            in >> stud.Vardas >> stud.Pavarde;
+            
+            string vardas, pavarde;
+            in >> vardas >> pavarde;
+
+            stud.set_Vardas(vardas); 
+            stud.set_Pavarde(pavarde);
             int pazimys;
             vector <int> pazymiai;
             while (in >> pazimys && pazimys >= 0)
@@ -121,47 +156,27 @@ class studentas {
 
         }
 
+        void set_egz(int egz){            egz_rez = egz;        }
 
-        void set_egz(int egz){
-            egz_rez = egz;
-        }
-        double get_egz(){
-            return egz_rez;
-        }
+        double get_egz(){            return egz_rez;        }
 
-        void set_tarpiniai(vector<int> tarp){
-            tarpiniai = tarp;
-        }
-        vector <int> get_tarpiniai( ){
-            return tarpiniai;
-        }
+        void set_tarpiniai(vector<int> tarp){            tarpiniai = tarp;        }
 
-        void set_paz_kiekis(int paz){
-            pazKiekis = paz;
-            
-        }
-        int get_paz_kieki(){
-            return pazKiekis;
-        }
+        vector <int> get_tarpiniai( ){            return tarpiniai;        }
 
-        void set_vidurkis(int vidur){
-            vidurkis = vidur;
-        }
+        void set_paz_kiekis(int paz){            pazKiekis = paz;                 }
 
-        void set_vidurkis(){
-            vidurkis = accumulate(tarpiniai.begin(), tarpiniai.end(), 0.0) / tarpiniai.size();
-        }
+        int get_paz_kieki(){            return pazKiekis;        }
 
-        double get_vidurkis(){
-            return vidurkis;
-        }
+        void set_vidurkis(int vidur){            vidurkis = vidur;        }
 
-        void set_mediana(int medi){
-            mediana = medi;
-        }
-        double get_mediana(){
-            return mediana;
-        }
+        void set_vidurkis(){            vidurkis = accumulate(tarpiniai.begin(), tarpiniai.end(), 0.0) / tarpiniai.size();        }
+
+        double get_vidurkis(){     return vidurkis;        }
+
+        void set_mediana(int medi){  mediana = medi;    }
+
+        double get_mediana(){  return mediana; }
 
         void set_gal_v_m (){
            
@@ -188,22 +203,210 @@ class studentas {
             if (pasirinkimas == 1) return gal_med ;
         }
 
-        string set_Vardas (string var){
-            Vardas = var;
-        }
-        string get_Vardas ()const{
-            return Vardas;
-        }
+};
 
-        string set_Pavarde (string pav){
-            Pavarde = pav;
-        }
-        string get_Pavarde ()const{
-            return Pavarde;
-        }
+
+// class studentas {
+//     private:
+    
+//         string Vardas;
+//         string Pavarde;
+//         int egz_rez;
+//         int pazKiekis;
+//         vector<int> tarpiniai;
+//         double vidurkis;
+//         double mediana;
+//         double gal_vid;
+//         double gal_med;
+
+//     public:
+//         studentas () : Vardas ( ""), Pavarde ( ""), tarpiniai (), pazKiekis (0), egz_rez (0){}
+
+//         studentas( string Var, string Pav, vector<int> tarp,double egz ) {
+//             Vardas = Var;
+//             Pavarde = Pav;
+//             tarpiniai = tarp;
+//             pazKiekis = tarp.size();
+//             egz_rez = egz;
+//             pazKiekis = tarpiniai.size();
+
+//         }
+
+//         /// destruktorius
+//         ~studentas ( ){tarpiniai.clear(), Vardas.clear(),Pavarde.clear();}
+
+//         /// kopijavimo konstruktorius
+//         studentas(const studentas& other) {
+//             Vardas = other.Vardas;
+//             Pavarde = other.Pavarde;
+//             egz_rez = other.egz_rez;
+//             tarpiniai = other.tarpiniai;
+//             vidurkis = other.vidurkis;
+//             mediana = other.mediana;
+//             gal_vid = other.gal_vid;
+//             gal_med = other.gal_med;
+//         }
+
+//         /// kopijavimo assignment operatorius
+//         studentas& operator=(const studentas& other) {
+//             if (this != &other) {
+//                 Vardas = other.Vardas;
+//                 Pavarde = other.Pavarde;
+//                 egz_rez = other.egz_rez;
+//                 tarpiniai = other.tarpiniai;
+//                 vidurkis = other.vidurkis;
+//                 mediana = other.mediana;
+//                 gal_vid = other.gal_vid;
+//                 gal_med = other.gal_med;
+//             }
+//             return *this;
+//         }
+
+//         /// perkelimo konstruktorius
+//         studentas(studentas&& other) noexcept {
+//             Vardas = move(other.Vardas);
+//             Pavarde = move(other.Pavarde);
+//             egz_rez = other.egz_rez;
+//             tarpiniai = move(other.tarpiniai);
+//             vidurkis = other.vidurkis;
+//             mediana = other.mediana;
+//             gal_vid = other.gal_vid;
+//             gal_med = other.gal_med;
+//         }
+
+//         /// perkelimo assignment operatorius
+//         studentas& operator=(studentas&& other) noexcept {
+//             if (this != &other) {
+//                 Vardas = move(other.Vardas);
+//                 Pavarde = move(other.Pavarde);
+//                 egz_rez = other.egz_rez;
+//                 tarpiniai = move(other.tarpiniai);
+//                 vidurkis = other.vidurkis;
+//                 mediana = other.mediana;
+//                 gal_vid = other.gal_vid;
+//                 gal_med = other.gal_med;
+//             }
+//             return *this;
+//         }
+
+//         friend ostream& operator<<(ostream& out, const studentas &stud) {                ///isvesties operatorius
+//         out << left << setw(15) << stud.Vardas << setw(15) << stud.Pavarde;
+//         out << setw(15) << fixed << setprecision(2) << stud.get_gal_v_m(0);
+//         out << setw(20) << setprecision(2) << stud.get_gal_v_m(1) << "\n";
+//         return out;
+//         }
+
+//         friend ostream& su_mediana(ostream& out, const studentas& stud) {
+//             out << left << setw(15) << stud.Vardas << setw(15) << stud.Pavarde;
+//             out << setw(20) << setprecision(2) << stud.gal_med << "\n";
+//             return out;
+//         }
+
+//         friend ostream& su_vidurkiu(ostream& out, const studentas& stud) {
+//             out << left << setw(15) << stud.Vardas << setw(15) << stud.Pavarde;
+//             out << setw(20) << setprecision(2) << stud.gal_vid<< "\n";
+//             return out;
+//         }
+
+
+//         friend istream& operator>>(istream& in, studentas &stud) {                      /// ivesties operatorius
+//             in >> stud.Vardas >> stud.Pavarde;
+//             int pazimys;
+//             vector <int> pazymiai;
+//             while (in >> pazimys && pazimys >= 0)
+//             {
+//                 pazymiai.push_back(pazimys);
+//             }
+//             stud.egz_rez=pazymiai.back() ;
+//             pazymiai.pop_back();
+
+//             stud.tarpiniai = pazymiai;
+
+//         }
+
+
+//         void set_egz(int egz){
+//             egz_rez = egz;
+//         }
+//         double get_egz(){
+//             return egz_rez;
+//         }
+
+//         void set_tarpiniai(vector<int> tarp){
+//             tarpiniai = tarp;
+//         }
+//         vector <int> get_tarpiniai( ){
+//             return tarpiniai;
+//         }
+
+//         void set_paz_kiekis(int paz){
+//             pazKiekis = paz;
+            
+//         }
+//         int get_paz_kieki(){
+//             return pazKiekis;
+//         }
+
+//         void set_vidurkis(int vidur){
+//             vidurkis = vidur;
+//         }
+
+//         void set_vidurkis(){
+//             vidurkis = accumulate(tarpiniai.begin(), tarpiniai.end(), 0.0) / tarpiniai.size();
+//         }
+
+//         double get_vidurkis(){
+//             return vidurkis;
+//         }
+
+//         void set_mediana(int medi){
+//             mediana = medi;
+//         }
+//         double get_mediana(){
+//             return mediana;
+//         }
+
+//         void set_gal_v_m (){
+           
+//             gal_vid = vidurkis * 0.4 + 0.6 *egz_rez;
+//             pazKiekis = tarpiniai.size();
+    
+//             sort(tarpiniai.begin(), tarpiniai.end());
+
+//             if ((pazKiekis % 2) == 0)
+//                 {
+//                     mediana = (double(tarpiniai[pazKiekis / 2 - 1]) + (tarpiniai[pazKiekis / 2])) / 2;
+//                 }
+//             else
+//                 {
+//                     mediana = (tarpiniai[pazKiekis / 2]);
+//                 }
+
+//                 gal_med = mediana * 0.4 + 0.6 *egz_rez;
+            
+
+//         }
+//         double get_gal_v_m (int pasirinkimas) const{
+//             if (pasirinkimas == 0) return gal_vid;
+//             if (pasirinkimas == 1) return gal_med ;
+//         }
+
+//         string set_Vardas (string var){
+//             Vardas = var;
+//         }
+//         string get_Vardas ()const{
+//             return Vardas;
+//         }
+
+//         string set_Pavarde (string pav){
+//             Pavarde = pav;
+//         }
+//         string get_Pavarde ()const{
+//             return Pavarde;
+//         }
 
         
-};
+// };
 
 /// ivairus vyriski vardai ir pavardes
 const vector<string> vyrV = {"Jonas", "Petras", "Antanas", "Juozas","Arnas","Dziugas", "Mantas", "Stasius","Vilius", "Kazimieras", "Algirdas", "Rimas", "Mindaugas", "Rokas", "Paulius","Kajus", "Pijus", "Titas"};
