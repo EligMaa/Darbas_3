47d4ae39a0b94d04629794c8ae6f18186ad95547
Rugile Ma <151032480+EligMaa@users.noreply.github.com>
2024-05-15 21:02:16 +0300

    Add files via upload

--- vektorius.h
+++ vektorius.h
@@ -14,8 +14,8 @@ template <typename T>
     {
         private:
             T *r_masyvas;        /// Rodiklis į masyvą.
-            size_t dydis;      /// Dabartinis vektoriaus talpumas.
-            size_t kiekis;      /// Dabartinis elementų skaičius vektoriuje.
+            size_t dydis;        /// Dabartinis vektoriaus talpumas.
+            size_t kiekis;       /// Dabartinis elementų skaičius vektoriuje.
 
         public:
   
@@ -25,11 +25,32 @@ template <typename T>
             // Konstruktorius
             VEKTORIUS() : r_masyvas(nullptr), dydis(0), kiekis(0) {}
 
+            VEKTORIUS(std::initializer_list<T> init_list) {
+                dydis = init_list.size();
+                kiekis = dydis;
+                r_masyvas = new T[kiekis];
+                std::copy(init_list.begin(), init_list.end(), r_masyvas);
+            }
+
             // Dekstruktorius
             ~VEKTORIUS()
             {
                 delete[] r_masyvas;
             }
+
+            pointer begin()
+            {
+                rodykle it;
+                it = (&r_masyvas[0]);
+                return it;
+            }
+            
+            pointer end()
+            {
+                rodykle it;
+                it = &r_masyvas[dydis - 1];
+                return it;
+            };
             
             // Kopijavimo konstruktorius
             VEKTORIUS(const VEKTORIUS& v) { create(v.begin(), v.end()); } 
@@ -95,8 +116,154 @@ template <typename T>
                 r_masyvas = new T[kiekis];
             }
 
+            size_t size() const { return dydis; }
+            size_t capacity() const { return kiekis; }
 
+            void erase(size_t val) {
+                if (val < 0 || val >= dydis)
+                    return;
+                dydis--;
+                std::move(&r_masyvas[val + 1], &r_masyvas[dydis], &r_masyvas[val]);
+            }
 
+            // grazin iteratori i pradzia
+            T* begin() {
+                return r_masyvas;
+            }
+
+            T* end() {
+                return (r_masyvas + dydis);
+            }
+
+            const T* begin() const {
+                return r_masyvas;
+            }
+
+            const T* end() const {
+                return r_masyvas + dydis;
+            }
+
+            void sort()
+            {
+                sort(begin(), end());
+            }
+
+            template <typename lyginti>
+            void sort(lyginti comp)
+            {
+                sort(begin(), end(), comp);
+            }
+                    
+            void resize(size_t naujas_kiekis) {
+                T* naujas_masyvas = new T[naujas_kiekis];
+                move(r_masyvas, r_masyvas + dydis, naujas_masyvas);
+                delete[] r_masyvas;
+                r_masyvas = naujas_masyvas;
+                kiekis = naujas_kiekis;
+            }
+
+            // Rezervuoti vietą elementams
+            void reserve(size_t naujas_kiekis)
+            {
+                if (naujas_kiekis > dydis)
+                {
+                    T *duomenys = new T[naujas_kiekis];
+                    for (size_t i = 0; i < kiekis; ++i)
+                    {
+                        duomenys[i] = std::move(r_masyvas[i]);
+                    }
+                    delete[] r_masyvas;
+                    r_masyvas = duomenys;
+                    dydis = naujas_kiekis;
+                }
+            }
+
+            // Ar vektorius yra tuščias
+            bool empty() const
+            {
+                return kiekis == 0;
+            }
+
+            void reverse()
+            {
+                reverse(begin(), end());
+            }
+
+            // Elementas pagal indeksą []
+            T &operator[](size_t indeksas)
+            {
+                if (indeksas >= kiekis)
+                    throw std::out_of_range("Indeksas už ribų");
+                return r_masyvas[indeksas];
+            }
+
+            // Pirmas elementas
+            T &front()
+            {
+                if (kiekis == 0)
+                    throw std::out_of_range("Vektorius yra tuščias");
+                return r_masyvas[0];
+            }
+
+            // // Paskutinis elementas
+            T& back() {
+                if (dydis == 0) {
+                    throw std::out_of_range("VEKTORIUS tuscias");
+                }
+                return r_masyvas[dydis - 1];
+            }
+
+            // Elemento iterpimas į vektorių
+            T *insert(iterator pozicija, const T &reiksme)
+            {
+                size_t indeksas = pozicija - begin();
+                if (kiekis >= dydis)
+                {
+                    size_t naujas_kiekis;
+                    if (dydis == 0) {
+                        naujas_kiekis = 1;
+                    } else {
+                        naujas_kiekis = dydis * 2;
+                    }
+
+                    reserve(naujas_kiekis);
+                }
+                std::move_backward(begin() + indeksas, end(), end() + 1);
+                r_masyvas[indeksas] = reiksme;
+                ++kiekis;
+                return begin() + indeksas;
+            }          
+            
+            // Pirmas pasikartojantis elementas vektoriuje
+            T &first_duplicate()
+            {
+                for (size_t i = 0; i < kiekis; ++i)
+                {
+                    for (size_t j = i + 1; j < kiekis; ++j)
+                    {
+                        if (r_masyvas[i] == r_masyvas[j])
+                        {
+                            return r_masyvas[i];
+                        }
+                    }
+                }
+                throw std::logic_error("Vektorius neturi pasikartojančių elementų");
+            }
+
+            // Elementu sk, kurie atitinka sąlyga
+            template <typename Predicate>
+            size_t count_if(Predicate pred)
+            {
+                size_t sum = 0;
+                for (const auto &element : *this)
+                {
+                    if (pred(element))
+                    {
+                        ++sum;
+                    }
+                }
+                return sum;
+            }
     };
 
 #endif // VEKTORIUS_H
